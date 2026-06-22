b7b01b09590bd31deaf39b835d69261d0bd7ccc6
sebastian <joan.orozco@globant.com>
2026-06-22 14:26:14 -0500

    update backend file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,13 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+   }
+  # Con esta configuración, Terraform guardará el .tfstate en la nube de Azure
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state-prod" # El grupo que creamos en el Paso 1
+    storage_account_name = "remotestatetest00"          # El nombre de tu Storage Account
+    container_name       = "tfstate"                  # El nombre del contenedor
+    key                  = "laboratorio.tfstate"       # El nombre físico que tendrá el archivo de estado
   }
 }
 
