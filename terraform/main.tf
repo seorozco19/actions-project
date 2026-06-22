972cce23cd019af6a5a244ee144dab5bb9adb4be
sebastian <joan.orozco@globant.com>
2026-06-22 14:38:50 -0500

    update Standard_B2s VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,7 +80,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-devops-lab"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s" # Tamaño económico para el lab
+  size                = "Standard_B2s" # Tamaño económico para el lab
   admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.nic.id,
