31b3844efa4bd4448bd87180d762c153afbf3f22
sebastian <joan.orozco@globant.com>
2026-06-23 10:52:37 -0500

    update pipeline and variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,4 @@
 variable "ssh_public_key" {
   type        = string
   description = "Contenido de la llave pública SSH para la máquina virtual"
-  default     = "" # Lo dejamos vacío por defecto
 }
