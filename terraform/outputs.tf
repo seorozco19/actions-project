b351504050f53b964953a24331b9fccf60deebfe
sebastian <joan.orozco@globant.com>
2026-06-22 22:10:30 -0500

    update deploy yaml

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "vm_public_ip" {
-  value       = azurerm_linux_virtual_machine.vm.public_ip_address
+  value       = azurerm_public_ip.pip.ip_address
   description = "La IP pública generada para conectar Ansible"
 }
